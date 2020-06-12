faa8c820e57dceb69d7bb70c4884253d6877cea2
suzukirisa <suzukirisa@192.168.0.11>
2020-06-12 15:20:00 +0900

    ten

---
+++ melon2/src/melon2/IntToEng.java
@@ -0,0 +1,29 @@
+package melon2;
+
+import java.util.Scanner;
+public class IntToEng {
+    // メインメソッド
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        System.out.println(translateEng(input));
+    }
+
+    // 数値を英訳する変換するメソッド
+    static String translateEng(int n) {
+    	String output="a";
+    	if(n==0) output="zero";
+    	if(n==1) output="one";
+    	if(n==2) output="two";
+    	if(n==3) output="three";
+    	if(n==4) output="four";
+    	if(n==5) output="five";
+    	if(n==6) output="six";
+    	if(n==7) output="seven";
+    	if(n==8) output="eight";
+    	if(n==9) output="nine";
+    	if(n==10) output="ten";
+        return output;
+    }
+}
+
